d4e2315a69e48a5d4e8cc9f96e15a43343fef2e0
gregwhitaker <gwhitake@gmail.com>
2020-01-17 13:28:14 -0800

    Initial Commit

---
+++ count-client/src/main/java/example/count/client/CountClient.java
@@ -0,0 +1,12 @@
+package example.count.client;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CountClient {
+    private static final Logger LOG = LoggerFactory.getLogger(CountClient.class);
+
+    public static void main(String... args) {
+        
+    }
+}
